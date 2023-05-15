08dd1f0e44607b17dac837a0e15980a69f0b527a
Hoon94 <dleognsdl1@naver.com>
2023-05-15 23:50:14 +0900

    Lv.0: 숫자 비교하기

---
+++ 프로그래머스/숫자 비교하기.swift
@@ -0,0 +1,14 @@
+//
+//  숫자 비교하기.swift
+//  SwiftAlgorithm
+//
+//  Created by Daehoon Lee on 2023/05/15.
+//
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    if num1 == num2 {
+        return 1
+    }
+    
+    return -1
+}
