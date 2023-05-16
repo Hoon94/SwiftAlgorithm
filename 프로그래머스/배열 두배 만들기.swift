cb235bc5ff81dd2126c4ce483229da79a0ddf72a
Hoon94 <dleognsdl1@naver.com>
2023-05-17 02:37:23 +0900

    Lv.0: 배열 두배 만들기

---
+++ 프로그래머스/배열 두배 만들기.swift
@@ -0,0 +1,10 @@
+//
+//  배열 두배 만들기.swift
+//  SwiftAlgorithm
+//
+//  Created by Daehoon Lee on 2023/05/17.
+//
+
+func solution(_ numbers:[Int]) -> [Int] {
+    return numbers.map { $0 * 2 }
+}
