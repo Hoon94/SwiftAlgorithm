d9b5daf762f165f4368c29bf3b63181c6b7e5934
Hoon94 <dleognsdl1@naver.com>
2023-05-19 23:31:13 +0900

    Lv.0: 중앙값 구하기

---
+++ 프로그래머스/중앙값 구하기.swift
@@ -0,0 +1,10 @@
+//
+//  중앙값 구하기.swift
+//  SwiftAlgorithm
+//
+//  Created by Daehoon Lee on 2023/05/19.
+//
+
+func solution(_ array:[Int]) -> Int {
+    return array.sorted()[array.count / 2]
+}
