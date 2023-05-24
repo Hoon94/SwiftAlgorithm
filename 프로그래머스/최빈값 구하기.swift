97324f8742c453c488bcc6b17b62f7177dc27c9f
Hoon94 <dleognsdl1@naver.com>
2023-05-24 23:39:43 +0900

    Lv.0: 최빈값 구하기

--- 프로그래머스/최빈값 구하기.swift
+++ 프로그래머스/최빈값 구하기.swift
@@ -6,28 +6,7 @@
 //
 
 func solution(_ array:[Int]) -> Int {
-    var dict: Dictionary<Int, Int> = [:]
-    var num = 0
-    var result = 0
-    var flag = true
+    let sorted = Dictionary(grouping: array) { $0 }.sorted { $0.value.count > $1.value.count }
     
-    for i in array {
-        if let key = dict[i] {
-            dict[i] = key + 1
-        } else {
-            dict[i] = 1
-        }
-    }
-    
-    for (k, v) in dict {
-        if num < v {
-            result = k
-            num = v
-            flag = true
-        } else if num == v {
-            flag = false
-        }
-    }
-    
-    return flag ? result : -1
+    return sorted.count > 1 && sorted[0].value.count == sorted[1].value.count ? -1 : sorted[0].key
 }
