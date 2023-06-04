c1dcca2fe27c16dfb84403c88a05743ea5355488
Hoon94 <dleognsdl1@naver.com>
2023-06-04 23:37:22 +0900

    Lv.0: 아이스 아메리카노

---
+++ 프로그래머스/아이스 아메리카노.swift
@@ -0,0 +1,10 @@
+//
+//  아이스 아메리카노.swift
+//  SwiftAlgorithm
+//
+//  Created by Daehoon Lee on 2023/06/04.
+//
+
+func solution(_ money:Int) -> [Int] {
+    return [money / 5500, money % 5500]
+}
