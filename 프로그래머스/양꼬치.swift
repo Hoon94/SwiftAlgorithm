e217195f601c10446c71d3d73750f37b33996d73
Hoon94 <dleognsdl1@naver.com>
2023-06-23 23:53:36 +0900

    Lv.0: 양꼬치

---
+++ 프로그래머스/양꼬치.swift
@@ -0,0 +1,10 @@
+//
+//  양꼬치.swift
+//  SwiftAlgorithm
+//
+//  Created by Daehoon Lee on 2023/06/23.
+//
+
+func solution(_ n:Int, _ k:Int) -> Int {
+    return (12000 * n) + (2000 * (k - n / 10))
+}
