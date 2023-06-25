e4e6524514e625ca6902a7ed409cc85cd2f47ed3
Hoon94 <dleognsdl1@naver.com>
2023-06-25 23:54:42 +0900

    Lv.0: 배열 자르기

---
+++ 프로그래머스/배열 자르기.swift
@@ -0,0 +1,10 @@
+//
+//  배열 자르기.swift
+//  SwiftAlgorithm
+//
+//  Created by Daehoon Lee on 2023/06/25.
+//
+
+func solution(_ numbers:[Int], _ num1:Int, _ num2:Int) -> [Int] {
+    return Array(numbers[num1...num2])
+}
