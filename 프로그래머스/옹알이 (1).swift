cb2c36408074fdd2f529a77c8b7776ecae5f21f2
Hoon94 <dleognsdl1@naver.com>
2023-09-27 23:58:20 +0900

    Lv.0: 옹알이 (1)

--- 프로그래머스/옹알이 (1).swift
+++ 프로그래머스/옹알이 (1).swift
@@ -6,7 +6,9 @@
 //
 
 func solution(_ babbling:[String]) -> Int {
-    var list: [String] = babbling.map { $0.replacingOccurrences(of: "aya", with: " ").replacingOccurrences(of: "ye", with: " ").replacingOccurrences(of: "woo", with: " ").replacingOccurrences(of: "ma", with: " ").replacingOccurrences(of: " ", with: "") }
-    
-    return list.filter { $0.count == 0}.count
+    return babbling.filter{ word in
+        var word = word
+        ["woo", "ye", "ma", "aya"].forEach{word = word.replacingOccurrences(of: $0, with: "")}
+        return word.isEmpty
+    }.count
 }
