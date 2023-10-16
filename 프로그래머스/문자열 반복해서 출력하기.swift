6b01cfa6124a7409af5159eb61effa19b0f987da
Hoon94 <dleognsdl1@naver.com>
2023-10-16 23:58:20 +0900

    Lv.0: 문자열 반복해서 출력하기

--- 프로그래머스/문자열 반복해서 출력하기.swift
+++ 프로그래머스/문자열 반복해서 출력하기.swift
@@ -10,6 +10,4 @@ import Foundation
 let inp = readLine()!.components(separatedBy: [" "]).map { $0 }
 let (s1, a) = (inp[0], Int(inp[1])!)
 
-for _ in 1...a {
-    print(s1, terminator: "")
-}
+print(String(repeating: s1, count: a))
