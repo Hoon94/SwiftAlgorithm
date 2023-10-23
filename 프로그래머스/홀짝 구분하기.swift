f16ce00f6afa486241f7fc9f19bfa758e474bbb1
Hoon94 <dleognsdl1@naver.com>
2023-10-23 23:20:17 +0900

    Lv.0: 홀짝 구분하기

---
+++ 프로그래머스/홀짝 구분하기.swift
@@ -0,0 +1,9 @@
+//
+//  홀짝 구분하기.swift
+//  SwiftAlgorithm
+//
+//  Created by Daehoon Lee on 2023/10/23.
+//
+
+let a = Int(readLine()!)!
+a % 2 == 0 ? print("\(a) is even") : print("\(a) is odd")
