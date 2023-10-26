564e329f072339dca038514ab12052ff0d1f096a
Hoon94 <dleognsdl1@naver.com>
2023-10-26 23:56:17 +0900

    Lv.0: 문자열 섞기

--- 프로그래머스/문자열 섞기.swift
+++ 프로그래머스/문자열 섞기.swift
@@ -6,14 +6,5 @@
 //
 
 func solution(_ str1: String, _ str2: String) -> String {
-    var answer = ""
-    var str_list1 = Array(str1)
-    var str_list2 = Array(str2)
-    
-    for i in 0...str1.count - 1 {
-        answer += String(str_list1[i])
-        answer += String(str_list2[i])
-    }
-    
-    return answer
+    return zip(str1, str2).map { String($0) + String($1) }.joined()
 }
