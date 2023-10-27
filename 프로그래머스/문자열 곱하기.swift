05b0711eab6d86bbc77cff9d74570c8202684b33
Hoon94 <dleognsdl1@naver.com>
2023-10-28 00:06:16 +0900

    Lv.0: 문자열 곱하기

---
+++ 프로그래머스/문자열 곱하기.swift
@@ -0,0 +1,10 @@
+//
+//  문자열 곱하기.swift
+//  SwiftAlgorithm
+//
+//  Created by Daehoon Lee on 2023/10/28.
+//
+
+func solution(_ my_string:String, _ k:Int) -> String {
+    return String(repeating: my_string, count: k)
+}
