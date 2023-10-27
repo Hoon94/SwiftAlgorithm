ffd688463728c40117df8f00fc13453d3f7bd407
Hoon94 <dleognsdl1@naver.com>
2023-10-27 23:43:15 +0900

    Lv.0: 문자 리스트를 문자열로 변환하기

---
+++ 프로그래머스/문자 리스트를 문자열로 변환하기.swift
@@ -0,0 +1,10 @@
+//
+//  문자 리스트를 문자열로 변환하기.swift
+//  SwiftAlgorithm
+//
+//  Created by Daehoon Lee on 2023/10/27.
+//
+
+func solution(_ arr:[String]) -> String {
+    return arr.joined()
+}
