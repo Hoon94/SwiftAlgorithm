40766109d6ac6ad89d702b74422c7045de330558
Hoon94 <dleognsdl1@naver.com>
2023-11-04 22:09:55 +0900

    Lv.0: 홀짝에 따라 다른 값 반환하기

--- 프로그래머스/홀짝에 따라 다른 값 반환하기.swift
+++ 프로그래머스/홀짝에 따라 다른 값 반환하기.swift
@@ -6,25 +6,9 @@
 //
 
 func solution(_ n:Int) -> Int {
-    var result = [Int]()
-    
     if n % 2 == 0 {
-        var i = 0
-        
-        while i <= n {
-            result.append(i*i)
-            i += 2
-        }
-        
-        return result.reduce(0, {x, y in x + y})
+        return stride(from: 2, through: n, by: 2).reduce(0) { $0 + $1 * $1 }
     } else {
-        var i = 1
-        
-        while i <= n {
-            result.append(i)
-            i += 2
-        }
-        
-        return result.reduce(0, {x, y in x + y})
+        return stride(from: 1, through: n, by: 2).reduce(0, +)
     }
 }
