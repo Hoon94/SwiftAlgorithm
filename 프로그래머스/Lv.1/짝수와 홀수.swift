8e324fc1c56d87be55a83fe7e00d12e79d4129f1
Hoon94 <dleognsdl1@naver.com>
2023-11-09 23:28:36 +0900

    Lv.1: 짝수와 홀수

---
+++ 프로그래머스/Lv.1/짝수와 홀수.swift
@@ -0,0 +1,10 @@
+//
+//  짝수와 홀수.swift
+//  SwiftAlgorithm
+//
+//  Created by Daehoon Lee on 2023/11/09.
+//
+
+func solution(_ num:Int) -> String {
+    return num % 2 == 0 ? "Even" : "Odd"
+}
