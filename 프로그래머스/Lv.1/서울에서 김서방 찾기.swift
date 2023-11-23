338bce5d7df4a3ceb449679361eef57ab62b28f3
Hoon94 <dleognsdl1@naver.com>
2023-11-23 23:53:28 +0900

    Lv.1: 서울에서 김서방 찾기

---
+++ 프로그래머스/Lv.1/서울에서 김서방 찾기.swift
@@ -0,0 +1,10 @@
+//
+//  서울에서 김서방 찾기.swift
+//  SwiftAlgorithm
+//
+//  Created by Daehoon Lee on 2023/11/23.
+//
+
+func solution(_ seoul:[String]) -> String {
+    return "김서방은 \(seoul.firstIndex(of: "Kim")!)에 있다"
+}
