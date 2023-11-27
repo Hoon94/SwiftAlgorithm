35f4dcef82d04317b1c1ce55dceeb5e34c788b75
Hoon94 <dleognsdl1@naver.com>
2023-11-27 15:51:44 +0900

    Lv.1: 없는 숫자 더하기

---
+++ 프로그래머스/Lv.1/없는 숫자 더하기.swift
@@ -0,0 +1,10 @@
+//
+//  없는 숫자 더하기.swift
+//  SwiftAlgorithm
+//
+//  Created by Daehoon Lee on 2023/11/27.
+//
+
+func solution(_ numbers:[Int]) -> Int {
+    return numbers.reduce(45, -)
+}
