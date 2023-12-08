7ba83d67a138e7fee6e283906121b583d6f6519c
Hoon94 <dleognsdl1@naver.com>
2023-12-09 00:33:07 +0900

    Lv.1: 문자열 내림차순으로 배치하기

---
+++ 프로그래머스/Lv.1/문자열 내림차순으로 배치하기.swift
@@ -0,0 +1,10 @@
+//
+//  문자열 내림차순으로 배치하기.swift
+//  SwiftAlgorithm
+//
+//  Created by Daehoon Lee on 2023/12/09.
+//
+
+func solution(_ s:String) -> String {
+    return String(s.sorted(by: >))
+}
