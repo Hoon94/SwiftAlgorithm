a4c5c57be16f8d66bc560a2bfb9ce7d030de7ac9
Hoon94 <dleognsdl1@naver.com>
2024-01-11 21:10:34 +0900

    Lv.1: 2016년

---
+++ 프로그래머스/Lv.1/2016년.swift
@@ -0,0 +1,13 @@
+//
+//  2016년.swift
+//  SwiftAlgorithm
+//
+//  Created by Daehoon Lee on 2024/01/11.
+//
+
+func solution(_ a:Int, _ b:Int) -> String {
+    let weekday = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
+    let daysInMonths = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    
+    return weekday[(daysInMonths[0..<a - 1].reduce(0, +) + b + 4) % 7]
+}
